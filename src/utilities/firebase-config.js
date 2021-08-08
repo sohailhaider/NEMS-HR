e3b68b02e40df327548631a02d3200a022647d0b
Sohail Haider <sohail.haider@synavos.com>
2021-08-09 00:54:53 +0500

    CONFIG moved to .env

--- src/utilities/firebase-config.js
+++ src/utilities/firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyC1OApSuruxSDjjYJKSwaKEE_9BBYMXJpQ",
-  authDomain: "hr-demo-2-f6311.firebaseapp.com",
-  projectId: "hr-demo-2-f6311",
-  storageBucket: "hr-demo-2-f6311.appspot.com",
-  messagingSenderId: "1057743240890",
-  appId: "1:1057743240890:web:a71dd5f4637af802b98c83",
-  measurementId: "G-Z11ZE245RF"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MID
 };
